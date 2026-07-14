f130c3ba5182c719c1066de44b478d4b02cbbb7b
Борис Бритва <BorisbritvaKGB@outlook.com>
2026-07-14 17:06:32 +0300

    lit-lit commit

--- Eureka-Server/src/main/java/Eureka/Server/Eureka/Listeners/EurekaStateLogger.java
+++ Eureka-Server/src/main/java/Eureka/Server/Eureka/Listeners/EurekaStateLogger.java
@@ -3,31 +3,26 @@ import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCance
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 public class EurekaStateLogger {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(YourClassName.class);
 
 	// 1. Ловим момент, когда любой сервис УСПЕШНО ПОДКЛЮЧИЛСЯ
 	@EventListener
 	public void onInstanceRegistered(EurekaInstanceRegisteredEvent event) {
-		String serviceName = event.getInstanceInfo().getAppName();
-		String ipAddress = event.getInstanceInfo().getIPAddr();
-		int port = event.getInstanceInfo().getPort();
-
-		System.out.println("\n" + "✅ ".repeat(15));
-		System.out.printf("[EUREKA] ПОДКЛЮЧИЛСЯ НОВЫЙ СЕРВИС: %s%n", serviceName);
-		System.out.printf("[INFO] Адрес: http://%s:%d%n", ipAddress, port);
-		System.out.println("✅ ".repeat(15) + "\n");
+		log.info("✅ Сервис {} зарегистрирован по адресу {}:{}",
+				event.getInstanceInfo().getAppName(),
+				event.getInstanceInfo().getIPAddr(),
+				event.getInstanceInfo().getPort());
 	}
 
 	// 2. Ловим момент, когда сервис ОТКЛЮЧИЛСЯ (или упал)
 	@EventListener
 	public void onInstanceCanceled(EurekaInstanceCanceledEvent event) {
-		String serviceName = event.getAppName();
-		String serverId = event.getServerId();
-
-		System.out.println("\n" + "❌ ".repeat(15));
-		System.out.printf("[EUREKA] СЕРВИС ОТКЛЮЧИЛСЯ/ПРОПАЛ: %s%n", serviceName);
-		System.out.printf("[INFO] ID сервера: %s%n", serverId);
-		System.out.println("❌ ".repeat(15) + "\n");
+		log.info("❌  Сервис {} отлючился",
+				event.getInstanceInfo().getAppName();
 	}
 }
